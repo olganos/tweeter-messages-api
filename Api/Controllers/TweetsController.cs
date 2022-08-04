da0eae5dd313be248df6a1bccd5f961b6d106326
Olga Nosova <olga.nosova@cognizant.com>
2022-08-04 18:49:33 +0200

    Created initial project

---
+++ Api/Controllers/TweetsController.cs
@@ -0,0 +1,33 @@
+﻿using Api.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/v1.0/tweets/[controller]")]
+    [ApiController]
+    public class TweetsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Tweet> Get()
+        {
+            var tweets = new List<Tweet> {
+                new Tweet
+                {
+                    Id =Guid.NewGuid(),
+                    Text = "Text",
+                },
+                new Tweet
+                {
+                    Id = Guid.NewGuid(),
+                    Text = "Text 2",
+                }};
+
+            return tweets;
+        }
+    }
+}
